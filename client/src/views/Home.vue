fde83a433456e74f5eb0d6b80ef42a4b7f69e0a4
Elijah Judge <elijahgjudge@gmail.com>
2020-10-15 22:31:37 -0400

    social feed added

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="home">
+  
+  <div class="page">
+      <h2 class="title is-2 level-item">Social</h2>
 
-    <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
-      
+    <div class="columns">
+
+        <div class="column is-two-thirds">
+          
+            <ExerciseCard /> <br>
+            <ExerciseCard /> <br>
+            <ExerciseCard /> <br>
+            <ExerciseCard /> <br>
+        </div>
+
+        <div class="column is-one-third" >
+            <Sidebar />
+
+        </div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
+import Sidebar from "@/components/Sidebar";
+import ExerciseCard from "@/components/ExerciseCard";
+
 
 export default {
   name: 'Home',
   components: {
-    
+    Sidebar, ExerciseCard
+
   }
 }
 </script>
+
